9115eb6cb75ee036a74e6f668925ff4fd9c85054
Mariia <marion177115@gmail.com>
2022-11-16 12:59:01 +0100

    movie

--- js/script.js
+++ js/script.js
@@ -1,22 +1,19 @@
-console.log('arr' + " - object");
-console.log(5 + + "2");
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-let incr = 10,
-    decr = 10;
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false,
+};
 
-	incr++;
-	decr--;
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+	b = prompt('На сколько оцените его?', ''),
+	c = prompt('Один из последних просмотренных фильмов?', ''),
+	d = prompt('На сколько оцените его?', '');
 
-	console.log(incr);
-	console.log(decr);
-	console.log(5%2);
-	console.log(2*4==8);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-	
-	const isChecked = true, 
-	      isClose = false;
-
-		  
-	 console.log(isChecked || !isClose);
-
-	
+console.log(personalMovieDB);
